199b6e034741924bad9fefeacdc350d84357e776
Ian Frosst <ianjfrosst@gmail.com>
2018-01-23 21:23:42 -0500

    Add some regex matches

--- discount/parser.py
+++ discount/parser.py
@@ -1,72 +1,75 @@
 
 import sys
+import re
 
 def parse(file_in, file_out):
     for line in file_in:
-        parseLine(line)
+        parse_line(line)
 
-def parseLine (line) :
-    headingDepth = 0
-    
-    if line.startswith("-") or line.startswith("*") :
-        print ("BULLET")
-    if line.startswith("#") :
-        headingDepth = 0
+def parse_line (line):
+    # let's match beginning-of-line elements
+    head = re.match(r'#+\ ?', line) # look for headings
+    quote = re.match(r'(>\ *)+', line) # look for quotes
+    if head: print(head)
+    if quote: print(quote)
+
+    heading_depth = 0
+
+    if line.startswith("-") or line.startswith("*"):
+        print("BULLET")
+
+    elif line.startswith("#"):
+        heading_depth = 0
         for a in line:
-            if a == "#" :
-                headingDepth += 1
+            if a == "#":
+                heading_depth += 1
             else:
                 break
-        print ("Heading, rank " + str(headingDepth))
-        line = line[headingDepth+1:]
-    if line.startswith("\\"):
-        print ("COMMENT: Print this exactly as shown!")
-    
-    
+        print("Heading, rank", heading_depth)
+        line = line[heading_depth+1:]
+
     lToken = "\0"
     tCount = 0
-    
+
     isComment = False
-    
-    def getTag(token,count) :
-        if token == "*" or token == "_" :
-            if count == 2 :
+
+    def get_tag(token,count):
+        if token == "*" or token == "_":
+            if count == 2:
                 return "<b>"
-                
-    
+
+
     for c in line:
         # Found a *; we're either
         
-        if c == lToken :
+        if c == lToken:
             tCount += 1
-        else :
+        else:
             tCount = 0
         
-        if c == "\\" :
+        if c == "\\":
             isComment = not isComment
             lToken = "\0"
             tCount = 0
         
-        if not isComment :
-            if c == "*" :
-                if lToken == c :
+        if not isComment:
+            if c == "*":
+                if lToken == c:
                     tCount += 1
-                else :
+                else:
                     tCount = 0
                 lToken = c
-            elif c == "_" :
-                if lToken == c :
+            elif c == "_":
+                if lToken == c:
                     tCount += 1
-                else :
+                else:
                     tCount = 0
                 lToken = c
             
-            if lToken == "*" or lToken == "_" :
-                if tCount == 1 :
-                    print ("<b>")
-                if tCount == 2 :
-                    print ("<i>")
-        
-    
+            if lToken == "*" or lToken == "_":
+                if tCount == 1:
+                    print("<b>")
+                if tCount == 2:
+                    print("<i>")
+
     print(line, end="")
-    pass

7398b453792657b371c656566b0291aa8548bc0d
Charles Holtforster <charles.holtforster@gmail.com>
2018-01-23 19:52:03 -0500

    Started on a token-based-ish parser

--- discount/parser.py
+++ discount/parser.py
@@ -6,17 +6,67 @@ def parse(file_in, file_out):
         parseLine(line)
     
 def parseLine (line) :
-    print(line, end="")
+    headingDepth = 0
+    
     if (line.startswith("-") or line.startswith("*")) :
         print ("BULLET")
     if (line.startswith("#")) :
-        rank = 0
+        headingDepth = 0
         for a in line:
             if (a == "#") :
-                rank += 1
+                headingDepth += 1
             else:
                 break
-        print ("Heading, rank " + str(rank))        
+        print ("Heading, rank " + str(headingDepth))
+        line = line[headingDepth+1:]
     if (line.startswith("\\")):
         print ("COMMENT: Print this exactly as shown!")
+    
+    
+    lToken = "\0"
+    tCount = 0
+    
+    isComment = False
+    
+    def getTag(token,count) :
+        if (token == "*" or token == "_") :
+            if (count == 2) :
+                return "<b>"
+                
+    
+    for c in line:
+        # Found a *; we're either
+        
+        if (c == lToken) :
+            tCount += 1
+        else :
+            tCount = 0
+        
+        if (c == "\\") :
+            isComment = not isComment
+            lToken = "\0"
+            tCount = 0;
+        
+        if (not isComment) :        
+            if (c == "*") :
+                if (lToken == c) :
+                    tCount += 1
+                else :
+                    tCount = 0
+                lToken = c
+            elif (c == "_") :
+                if (lToken == c) :
+                    tCount += 1
+                else :
+                    tCount = 0
+                lToken = c
+            
+            if (lToken == "*" or lToken == "_") :
+                if (tCount == 1) :
+                    print ("<b>")
+                if (tCount == 2) :
+                    print ("<i>")
+        
+    
+    print(line, end="")
     pass

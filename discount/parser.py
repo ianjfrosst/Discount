463190e7783ffbad5d3b9811677994befdfb7877
Charles Holtforster <charles.holtforster@gmail.com>
2018-01-23 20:07:02 -0500

    Changed if syntax Because someone asked nicely.

--- discount/parser.py
+++ discount/parser.py
@@ -8,18 +8,18 @@ def parse(file_in, file_out):
 def parseLine (line) :
     headingDepth = 0
     
-    if (line.startswith("-") or line.startswith("*")) :
+    if line.startswith("-") or line.startswith("*") :
         print ("BULLET")
-    if (line.startswith("#")) :
+    if line.startswith("#") :
         headingDepth = 0
         for a in line:
-            if (a == "#") :
+            if a == "#" :
                 headingDepth += 1
             else:
                 break
         print ("Heading, rank " + str(headingDepth))
         line = line[headingDepth+1:]
-    if (line.startswith("\\")):
+    if line.startswith("\\"):
         print ("COMMENT: Print this exactly as shown!")
     
     
@@ -29,42 +29,42 @@ def parseLine (line) :
     isComment = False
     
     def getTag(token,count) :
-        if (token == "*" or token == "_") :
-            if (count == 2) :
+        if token == "*" or token == "_" :
+            if count == 2 :
                 return "<b>"
                 
     
     for c in line:
         # Found a *; we're either
         
-        if (c == lToken) :
+        if c == lToken :
             tCount += 1
         else :
             tCount = 0
         
-        if (c == "\\") :
+        if c == "\\" :
             isComment = not isComment
             lToken = "\0"
-            tCount = 0;
+            tCount = 0
         
-        if (not isComment) :        
-            if (c == "*") :
-                if (lToken == c) :
+        if not isComment :
+            if c == "*" :
+                if lToken == c :
                     tCount += 1
                 else :
                     tCount = 0
                 lToken = c
-            elif (c == "_") :
-                if (lToken == c) :
+            elif c == "_" :
+                if lToken == c :
                     tCount += 1
                 else :
                     tCount = 0
                 lToken = c
             
-            if (lToken == "*" or lToken == "_") :
-                if (tCount == 1) :
+            if lToken == "*" or lToken == "_" :
+                if tCount == 1 :
                     print ("<b>")
-                if (tCount == 2) :
+                if tCount == 2 :
                     print ("<i>")
         
     

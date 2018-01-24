6b8d147949c73603cf0082bbef3d5b29951458ee
Charles Holtforster <charles.holtforster@gmail.com>
2018-01-24 10:15:39 -0500

    Added lists. Fixed format clearing.

--- discount/parser.py
+++ discount/parser.py
@@ -39,7 +39,7 @@ def parse2(file_in, file_out):
         token = "\0"
         count = 0
 
-        if line == "":
+        if line.splitlines()[0] == "":
             # Remove all formatting
             for form in formats:
                 print("</" + form + ">", end="")
@@ -47,7 +47,7 @@ def parse2(file_in, file_out):
             for form in lineFormats:
                 print("</" + form + ">", end="")
                 lineFormats.remove(form)
-            print("<br>",end="")
+            #print("<br>",end="")
 
 
         header = re.match(r'#+ ', line)
@@ -59,6 +59,10 @@ def parse2(file_in, file_out):
         if line.startswith("- ") or line.startswith("* "):
             lineFormats.append(line_format_from_token("-",1))
             line = line[2:]
+            if "ul" not in formats:
+                formats.append("ul")
+                print("<" + "ul" + ">", end="")
+
 
         if line.startswith("> "):
             lineFormats.append(line_format_from_token(">", 1))
